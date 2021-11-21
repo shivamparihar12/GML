5de717b77f546173155939fbc88f6177f5ce298d
shivamparihar12 <parihar.shivam0789@gmail.com>
2021-11-21 14:48:31 +0530

    basic structure is ready

---
+++ app/src/main/java/com/example/gml/UriViewModel.java
@@ -0,0 +1,10 @@
+package com.example.gml;
+
+import android.net.Uri;
+
+import androidx.lifecycle.ViewModel;
+
+public class UriViewModel extends ViewModel {
+
+    Uri imageUri=null,videoUri=null;
+}
